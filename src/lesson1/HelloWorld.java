34e9f1c300c329a72f976e7c322bf95766868498
Alexander Chernomyrdin <chav1961@mail.ru>
2020-09-29 10:56:00 +0500

    DAily commit

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -1,6 +1,10 @@
 package lesson1;
 
-public class HelloWorld /*extends java.lang.Object*/ {
+import java.io.InputStream;
+//import java.lang.*;
+//import lesson1.*;
+
+public class HelloWorld /*extends Object*/ {
 	protected static int x;
 	// - примитивные типы
 	// -- группа целых чисел
@@ -19,5 +23,61 @@ public class HelloWorld /*extends java.lang.Object*/ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.err.println("Hello world");
+		byte	b1 = 10, b2 = 20, b3 = (byte)(b1 + b2);
+		
+		System.err.println("b1+b2="+(b1+b2));
+		System.err.println("b1-b2="+(b1-b2));
+		System.err.println("b1*b2="+(b1*b2));
+		System.err.println("b1/b2="+(b1/b2));
+		System.err.println("b1%b2="+(b1%b2));
+
+		System.err.println("b1|b2="+(b1|b2));
+		System.err.println("b1&b2="+(b1&b2));
+		System.err.println("b1^b2="+(b1^b2));
+		System.err.println("~b1="+(~b1));
+		
+		System.err.println("b1<<2="+(b1<<2));
+		System.err.println("b1>>2="+((-b1)>>2));
+		System.err.println("b1>>>2="+((-b1)>>>2));
+		
+		System.err.println("b1<b2="+(b1<b2));
+		System.err.println("b1<=b2="+(b1<=b2));
+		System.err.println("b1>b2="+(b1>b2));
+		System.err.println("b1>=b2="+(b1>=b2));
+		System.err.println("b1==b2="+(b1==b2));
+		System.err.println("b1!=b2="+(b1!=b2));
+		
+		double	d1 = 10, d2 = 20, d3 = b1 + b2;
+		
+		System.err.println("d1+d2="+(d1+d2));
+		System.err.println("d1-d2="+(d1-d2));
+		System.err.println("d1*d2="+(d1*d2));
+		System.err.println("d1/d2="+(d1/d2));
+		System.err.println("d1%d2="+(d1%d2));
+
+//		System.err.println("d1|d2="+(d1|d2));
+//		System.err.println("d1&d2="+(d1&d2));
+//		System.err.println("d1^d2="+(d1^d2));
+//		System.err.println("~d1="+(~d1));
+//		
+//		System.err.println("d1<<2="+(d1<<2));
+//		System.err.println("d1>>2="+((-d1)>>2));
+//		System.err.println("d1>>>2="+((-d1)>>>2));
+		
+		System.err.println("d1<d2="+(d1<d2));
+		System.err.println("d1<=d2="+(d1<=d2));
+		System.err.println("d1>d2="+(d1>d2));
+		System.err.println("d1>=d2="+(d1>=d2));
+		System.err.println("d1==d2="+(d1==d2));
+		System.err.println("d1!=d2="+(d1!=d2));
+		
+		byte	x = 10;
+		int		x1 = -100, x2 = 0xFEDA, x3 = 0b0110, x4 = 1_000_000, x5 = 077;
+		long	val1 = 100, val2 = 1234567890123456L;
+		float	f1 = 0.35f, f2 = 1.5e-3f;
+		double	dd1 = 0.35, dd2 = 1.5e-3f;
+		char	c1 = 10, c2 = '\r', c3 = 'a', c4 = '\070' , c5 = '\u0070';
+		boolean	v1 = true, v2 = false;
+		 
 	}
 }
