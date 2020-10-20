5bb931293fb8104f91b3a804b3ffb9aebf14f8c9
Alexander Chernomyrdin <chav1961@mail.ru>
2020-10-20 10:25:16 +0500

    DAily commit

---
+++ src/lesson4/Exercises3.java
@@ -0,0 +1,31 @@
+package lesson4;
+
+public class Exercises3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LifeCycleInterface	lc = null;// ????
+		MoreLessInterface	ml = null;// ???
+		
+		lc.start();
+		System.err.println("Started");
+		lc.suspend();
+		System.err.println("Suspended");
+		lc.resume();
+		System.err.println("Resumed");
+		if (lc instanceof MoreLessInterface) {
+			((MoreLessInterface)lc).more();
+			System.err.println("More fan");
+			((MoreLessInterface)lc).less();
+			System.err.println("Less fan");
+		}
+		lc.stop();
+		System.err.println("Stopped");
+
+		ml.more();
+		System.err.println("More");
+		ml.less();
+		System.err.println("L");
+	}
+
+}
